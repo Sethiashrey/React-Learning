7ed46ac60285a17a98c405ccba95b8b0ff074684
Shrey Sethia <shreysethia4810@gmail.com>
2025-07-14 22:56:12 +0530

    React file added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+function Sunny(){
+  return(
+    <>
+      <h1>Shrey Sethia</h1>
+      <p>I am a good Boy</p>
+    </>
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+      <Sunny />
+    </>
+  )
+}
+
+export default App
