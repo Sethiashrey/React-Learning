36c1537c4eec516d1329d3bff6e631bb39031cc8
Shrey Sethia <shreysethia4810@gmail.com>
2025-07-16 22:50:04 +0530

    React file Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 
 import './App.css'
+import Boss from './Boss.jsx';
 function Sunny(){
+  let name  = "Shrey Sethia";
   return(
     <>
+       <h2>Name is : {name}</h2>
       <h1>Shrey Sethia</h1>
       <p>I am a good Boy</p>
+      
     </>
   )
 }
 
 function App() {
 
-
   return (
     <>
       <Sunny />
+      <Boss />
     </>
   )
 }
---
+++ src/Boss.jsx
@@ -0,0 +1,9 @@
+function Boss(){
+    return (
+        <>
+          <h1>This is the boss component</h1>
+          <p>Shrey is the boss</p>
+        </>
+    )
+}
+export default Boss;
